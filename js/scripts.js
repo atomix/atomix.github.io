f43c27990c048ef5d709a82ef5c3b21d81c67fb4
Jonathan Halterman <jhalterman@gmail.com>
2015-09-01 09:58:11 -0700

    Sidebar fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 /** scroll spy */
 $('body').scrollspy({ target: '#sidebar-col' })
 
+// make sidebar sticky when it hits the top of the viewport
+var $sideBar = $("#sidebar");
+$sideBar.affix({
+  offset: {
+    top: function () {
+      var offsetTop      = $sideBar.offset().top
+      var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+      var navOuterHeight = $('.navbar-fixed-top').height()
+
+      return (this.top = offsetTop - navOuterHeight - sideBarMargin - 20)
+    },
+  bottom: function () {
+      return (this.bottom = $('#footer').outerHeight(true) + 64)
+    }
+  }
+})
+
+$(window).resize(function() {
+    $sidebar.affix("checkPosition");
+});
+
 /** smooth scrolling */
 $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
